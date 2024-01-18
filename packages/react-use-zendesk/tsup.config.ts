99a72a46a7167f02993ac56219a68810b9abf32d
diego tonini <diego.tonini93@gmail.com>
2024-01-18 22:52:14 +0100

    fix: change build setting

--- packages/react-use-zendesk/tsup.config.ts
+++ packages/react-use-zendesk/tsup.config.ts
@@ -2,13 +2,13 @@ import type { Options } from "tsup";
 
 export const tsup: Options = {
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   external: ["react"],
   sourcemap: true,
   treeshake: true,
   clean: true,
   splitting: false,
   dts: true,
-  // minify: true, => put true when library is ready
+  minify: true, // => put true during development
   target: "es2017",
 };
